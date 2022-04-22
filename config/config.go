de0513a00d2bf5e669016ee9ff98320108d97ad9
Russell Gapasin <russell@fanswifi.com>
2022-04-22 23:08:59 +0800

    feat: Added get config method

--- config/config.go
+++ config/config.go
@@ -2,10 +2,34 @@ package config
 
 import (
   "log"
-  "github.com/spf13/viper"
+  "os"
+  "github.com/joho/godotenv"
 )
 
-func Config() {
-  viper.SetConfigName(".env")
+type Config struct {
+  Port       string
+  DBHost     string
+  DBUser     string
+  DBPassword string
+  DBName     string
+  DBPort     string
+  DBSslmode  string
+}
 
+func GetConfig() *Config {
+  err := godotenv.Load(".env.local")
+  if err != nil {
+    log.Fatalf("[ setConfig ] Error loading env file: %v", err)
+  }
+
+  config := Config{
+    Port: os.Getenv("PORT"),
+    DBHost: os.Getenv("DB_HOST"),
+    DBUser: os.Getenv("DB_USER"),
+    DBPassword: os.Getenv("DB_PASSWORD"),
+    DBName: os.Getenv("DB_NAME"),
+    DBPort: os.Getenv("DB_PORT"),
+    DBSslmode: os.Getenv("DB_SSLMODE"),
+  }
+  return &config
 }
